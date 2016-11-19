b1de37e05c27b6e8e42f2d94e10d3be009b59d02
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-19 13:01:53 +0100

    REFACTOR

--- BlackJack_Java/src/model/Card.java
+++ BlackJack_Java/src/model/Card.java
@@ -6,22 +6,27 @@ public class Card {
 	public enum Color {Hearts, Spades, Diamonds, Clubs, Hidden}
 	public enum Value {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Knight, Queen, King, Ace, Hidden}
 	
+	private Color m_color;
+	private Value m_value;
 	
+	public Card() {
+		
+	}
 	
 	public Card makeNewCard(Color a_color, Value a_value) {
-		// TODO Auto-generated method stub
-		return new Card();
+		return new Card(a_color, a_value);
 	}
 
-
 	public Color getColor() {
-		// TODO Auto-generated method stub
 		return Color.Hidden;
 	}
 
-
 	public Value getValue() {
-		// TODO Auto-generated method stub
 		return Value.Hidden;
+	}
+	
+	private Card(Color a_color, Value a_value){
+		m_color = a_color;
+		m_value = a_value;
 	}
 }
