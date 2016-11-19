63c85d272cef6d7c7740a1f33dff05c23ebc827b
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-19 19:49:36 +0100

    COVERAGE 97.9%

---
+++ BlackJack_Java/tests/model/AllTests.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CardTest.class, DeckTest.class, PlayerTest.class })
+public class AllTests {
+
+}
