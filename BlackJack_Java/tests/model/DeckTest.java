ae590857a30d3f9c61b2be25e46c6ab2dadf32fe
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-19 14:22:43 +0100

    RED

---
+++ BlackJack_Java/tests/model/DeckTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Card.Color;
+import model.Card.Value;
+
+public class DeckTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void shouldCreateAndReturnANewDeck() {
+		Card mockCard = mock(Card.class);
+		when(mockCard.makeNewCard(any(Card.Color.class), any(Card.Value.class))).
+		thenReturn(new Card().makeNewCard(any(Card.Color.class), any(Card.Value.class)));
+		
+		Deck sut = new Deck();
+		sut = sut.makeNewDeck(mockCard);
+		
+		assertNotNull(sut);
+		
+		verify(mockCard, times(52)).makeNewCard(any(Color.class), any(Value.class));
+		verify(mockCard, never()).makeNewCard(Color.Hidden, Value.Hidden);
+	}
+}
