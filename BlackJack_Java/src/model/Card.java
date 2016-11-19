2b8361daace9cd721de541894a1d27e811e1c53f
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-19 12:58:00 +0100

    GREEN

--- BlackJack_Java/src/model/Card.java
+++ BlackJack_Java/src/model/Card.java
@@ -7,20 +7,21 @@ public class Card {
 	public enum Value {Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Knight, Queen, King, Ace, Hidden}
 	
 	
+	
 	public Card makeNewCard(Color a_color, Value a_value) {
 		// TODO Auto-generated method stub
-		return null;
+		return new Card();
 	}
 
 
 	public Color getColor() {
 		// TODO Auto-generated method stub
-		return null;
+		return Color.Hidden;
 	}
 
 
 	public Value getValue() {
 		// TODO Auto-generated method stub
-		return null;
+		return Value.Hidden;
 	}
 }
