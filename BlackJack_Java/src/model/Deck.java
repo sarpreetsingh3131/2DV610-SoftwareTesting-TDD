c26b6cfac43286869b61b10cc1893892dd7cd649
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-19 13:35:01 +0100

    GREEN

--- BlackJack_Java/src/model/Deck.java
+++ BlackJack_Java/src/model/Deck.java
@@ -1,10 +1,16 @@
 package model;
 
+import model.Card.Color;
+import model.Card.Value;
+
 public class Deck {
 
 	public Deck makeNewDeck(Card card) {
 		// TODO Auto-generated method stub
-		return null;
+		for(int i = 0; i < 52; i++){
+			card.makeNewCard(Color.Clubs, Value.Ace);
+		}
+		return new Deck();
 	}
 
 }
