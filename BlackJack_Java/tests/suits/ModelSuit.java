4d1cb64ab90f238e06329839e23aa5e39827cd37
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-30 14:39:54 +0100

    COVERAGE 97.8%

--- BlackJack_Java/tests/suits/ModelSuit.java
+++ BlackJack_Java/tests/suits/ModelSuit.java
@@ -5,11 +5,12 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import model.CardTest;
+import model.DealerTest;
 import model.DeckTest;
 import model.PlayerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CardTest.class, DeckTest.class, PlayerTest.class })
+@SuiteClasses({ CardTest.class, DeckTest.class, PlayerTest.class, DealerTest.class })
 public class ModelSuit {
 
 }
--- BlackJack_Java/tests/suits/RulesSuit.java
+++ BlackJack_Java/tests/suits/RulesSuit.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import rules.AmericanNewGameRuleTest;
 import rules.BasicHitRuleTest;
 import rules.DealerWinRuleTest;
 import rules.RulesFactoryTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({RulesFactoryTest.class, DealerWinRuleTest.class, BasicHitRuleTest.class})
+@SuiteClasses({RulesFactoryTest.class, DealerWinRuleTest.class, BasicHitRuleTest.class, AmericanNewGameRuleTest.class})
 public class RulesSuit {
 
 }
