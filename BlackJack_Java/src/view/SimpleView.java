4b4f4d2861ef4e3cdd0014f9bb659c545b60d0de
Sarpreet Singh <sb223ce@student.lnu.se>
2016-11-30 21:03:47 +0100

    GREEN

--- BlackJack_Java/src/view/SimpleView.java
+++ BlackJack_Java/src/view/SimpleView.java
@@ -32,6 +32,10 @@ public class SimpleView {
 
 	public void displayPlayerHand(ArrayList<Card> spyList, int score) {
 		// TODO Auto-generated method stub
-		
+		printer.println("Player has: ");
+		for (int i = 0; i < spyList.size(); i++) {
+			printer.println("Ace of Clubs");
+		}
+		printer.println("Score: " + score + "\n");
 	}
 }
