2ab0cac7ee5e6024756f980e4b576d852dc2bba1
Sarpreet Singh <sb223ce@student.lnu.se>
2016-12-01 19:33:11 +0100

    REFACTOR

--- BlackJack_Java/src/controller/PlayGame.java
+++ BlackJack_Java/src/controller/PlayGame.java
@@ -6,10 +6,11 @@ import view.SimpleView;
 public class PlayGame {
 
 	public boolean play(SimpleView view, Game game) {
-		// TODO Auto-generated method stub
 		view.displayWelcomeMessage();
+		
 		view.displayDealerHand(game.getDealerHand(), game.getDealerScore());
 		view.displayPlayerHand(game.getPlayerHand(), game.getPlayerScore());
+		
 		return true;
 	}
 }
--- BlackJack_Java/tests/controller/PlayGameTest.java
+++ BlackJack_Java/tests/controller/PlayGameTest.java
@@ -8,12 +8,14 @@ import view.SimpleView;
 
 public class PlayGameTest {
 
+	PlayGame sut;
+	SimpleView mockView = mock(SimpleView.class);
+	Game mockGame = mock(Game.class);
+	
 	@Test
 	public void shouldReturnTrueAndDisplayWelcomeMessageDealerAndPlayerHand() {
-		PlayGame sut = new PlayGame();
-		SimpleView mockView = mock(SimpleView.class);
-		Game mockGame = mock(Game.class);
-
+		sut = new PlayGame();
+		
 		assertTrue(sut.play(mockView, mockGame));
 		verify(mockView, times(1)).displayWelcomeMessage();
 		verify(mockView, times(1)).displayDealerHand(mockGame.getDealerHand(), mockGame.getDealerScore());
