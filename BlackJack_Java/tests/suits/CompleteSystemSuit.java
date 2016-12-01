2f73e430d973f64c4d53365066eab3b8df19e13b
Sarpreet Singh <sb223ce@student.lnu.se>
2016-12-01 20:18:08 +0100

    COVERAGE 98.9%

--- BlackJack_Java/tests/suits/CompleteSystemSuit.java
+++ BlackJack_Java/tests/suits/CompleteSystemSuit.java
@@ -3,9 +3,11 @@ package suits;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+import controller.PlayGameTest;
+import view.SimpleViewTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ModelSuit.class, RulesSuit.class, ViewSuit.class })
+@SuiteClasses({ ModelSuit.class, RulesSuit.class, SimpleViewTest.class, PlayGameTest.class})
 public class CompleteSystemSuit {
 
 }
